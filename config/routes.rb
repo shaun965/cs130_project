72df985cb663a4e0b48f357cab32803aa14fd306
Shaun Liu <shaun965@hotmail.com>
2017-06-03 15:01:02 -0700

    Added routes patient and doctor authentication and doctor sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   devise_for :doctors
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root to: "home#index"
+
+	namespace :api do
+		namespace :v1 do
+		  post 'auth_patient' => 'patient#authenticate_patient'
+		  post 'auth_doctor' => 'doctor#authenticate_doctor'
+		  post 'signup_doctor' => 'doctor#sign_up'
+		end
+	end
+
 end
